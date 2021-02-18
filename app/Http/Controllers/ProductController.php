b5d429b898052348811af012592828ea8759fdcc
SiemenST <siemenstapel@gmail.com>
2021-02-18 16:15:17 +0100

    productcontroller and routing start

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+
+
+public function index()
+{
+
+    $products = Product::latest()->get();
+
+    return view('products.index', ['products' => $products]);
+
+}
+
 }
