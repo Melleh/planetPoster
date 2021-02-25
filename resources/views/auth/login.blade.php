c5bd38f964ddb36966e4567fd2f99505f9e48f8e
SVB94 <sandervanbenthem94@gmail.com>
2021-02-25 10:06:22 +0100

    authentication

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -23,6 +23,7 @@
             </div>
 
             <!-- Password -->
+
             <div class="mt-4">
                 <x-label for="password" :value="__('Password')" />
 
@@ -32,13 +33,16 @@
                                 required autocomplete="current-password" />
             </div>
 
+
             <!-- Remember Me -->
+
             <div class="block mt-4">
                 <label for="remember_me" class="inline-flex items-center">
                     <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
                     <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
                 </label>
             </div>
+
 
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
