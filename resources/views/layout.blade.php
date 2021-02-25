e10c3542a6ef8f84d41b7037c1000ece339b679a
melvin <melvinboelens@gmail.com>
2021-02-25 10:32:52 +0100

    commit

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -141,7 +141,6 @@
         <div id="copyright" class="container">
             <ul class="links">
                 <li>&copy; Untitled. All rights reserved.</li>
-                <li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
             </ul>
         </div>
     </section>
